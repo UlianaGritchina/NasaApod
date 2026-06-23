642e243269329f6cee2ab5456830cdf58f5acf69
JulianaNemo <ulianaezakmie@gmail.com>
2026-06-23 15:16:02 +0300

    Initial Commit.

---
+++ NASA_APOD/App/NASA_APODApp.swift
@@ -0,0 +1,19 @@
+//
+//  NASA_APODApp.swift
+//  NASA_APOD
+//
+//  Created by Ульяна Гритчина on 23.06.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NASA_APODApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
---
+++ NASA_APOD/Presentation/MainTabView/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  NASA_APOD
+//
+//  Created by Ульяна Гритчина on 23.06.2026.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
