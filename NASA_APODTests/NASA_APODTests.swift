64c61c1cf8cfca6d5b4376a32ec47c95c0bb1c2c
JulianaNemo <ulianaezakmie@gmail.com>
2026-06-23 15:09:29 +0300

    Initial Commit

---
+++ NASA_APODTests/NASA_APODTests.swift
@@ -0,0 +1,16 @@
+//
+//  NASA_APODTests.swift
+//  NASA_APODTests
+//
+//  Created by Ульяна Гритчина on 23.06.2026.
+//
+
+import Testing
+
+struct NASA_APODTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
