bd3326e1f59cd6893470e465ace4e48548843449
UlianaGritchina <ugritchina@icloud.com>
2026-07-03 17:28:52 +0300

    Project renamed.

--- NasaApod/App/Environment/NasaApiConfig.swift
+++ NasaApod/App/Environment/NasaApiConfig.swift
@@ -9,6 +9,6 @@ import Foundation
 
 enum NasaApiConfig: String {
     case baseURL = "https://api.nasa.gov"
-    case apiKey = "Your api key from: https://api.nasa.gov"
+    case apiKey = "zZKhKycbkZ9IJ9qEEHoyGMYp8v0epEfTJ9XfHyNn"
     case demoKey = "oWsYmdEdqaHzl1gwxBgU8V8Y8rLak8sFZgfgsH4W"
 }
